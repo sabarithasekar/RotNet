99caaba582162beeab5dbc1ad5a3952ec542ff55
Sabaritha Sekar <sabaritha.sekar@owllabs.com>
2023-07-13 11:39:14 -0700

    fixes to make it run on my mac

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from keras.preprocessing.image import Iterator
-from keras.utils.np_utils import to_categorical
+from keras.utils import to_categorical
 import keras.backend as K
 
 
